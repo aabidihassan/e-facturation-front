69302b9ed291a645f40ec7fe1a056a976cfb3f13
aabidihassan <aabidihassan2@gmail.com>
2022-10-09 20:24:46 +0100

    modele sizes fix

--- src/app/demo/components/modelefacture/modelefacture.component.ts
+++ src/app/demo/components/modelefacture/modelefacture.component.ts
@@ -39,14 +39,14 @@ export class ModelefactureComponent implements OnInit {
   ];
 
   tailles = [
-    { label: '8', value : '8' },
-    { label: '9' , value : '9'},
-    { label: '10' , value : '10'},
-    { label: '11' , value : '11'},
-    { label: '12' , value : '12'},
-    { label: '13' , value : '13'},
-    { label: '14' , value : '14'},
-    { label: '16' , value : '16'}
+    { label: '8', value : 8 },
+    { label: '9' , value : 9},
+    { label: '10' , value : 10},
+    { label: '11' , value : 11},
+    { label: '12' , value : 12},
+    { label: '13' , value : 13},
+    { label: '14' , value : 14},
+    { label: '16' , value : 16}
   ];
 
   polices = [
